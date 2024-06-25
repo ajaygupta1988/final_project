d1979e2bebc83daa668e254c349667a76fb5014f
Ajay Gupta <ajay_gupta@live.com>
2024-06-24 22:43:00 -0400

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -8,7 +8,7 @@ load_dotenv(dotenv_path=f"{BASE_DIR}/.env")
 
 
 class Settings(BaseSettings):
-    database_url: str = getenv("DATABASE_URL")
+    database_url: str = getenv("DATABASE_URL", "")
     database_tls: bool = getenv("DATABASE_TLS", True)
     database: str = getenv("DATABASE", "coredb")
     vantage_key: str = getenv("VANTAGE_KEY", "demo")
