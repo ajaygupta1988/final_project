a6e4e0f62ce415c415d3bfd7d0ed1ca8668f6f94
Ajay Gupta <ajay_gupta@live.com>
2024-06-24 22:37:15 -0400

    Update settings.py
    fixed env

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,17 +1,17 @@
 from pathlib import Path
+from os import getenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).parent.resolve()
+load_dotenv(dotenv_path=f"{BASE_DIR}/.env")
 
 
 class Settings(BaseSettings):
-    database_url: str
-    database_tls: bool
-    database: str
-    vantage_key: str
-
-    model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/.env")
+    database_url: str = getenv("DATABASE_URL")
+    database_tls: bool = getenv("DATABASE_TLS", True)
+    database: str = getenv("DATABASE", "coredb")
+    vantage_key: str = getenv("VANTAGE_KEY", "demo")
 
 
 settings = Settings()
